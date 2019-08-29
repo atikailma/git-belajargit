3645bec2051e5b5be3f3f1932e43356718692dd3
atikailma <atikailma28@gmail.com>
2019-08-29 11:18:16 +0700

    git-belajargit

---
+++ home.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<style>
+   #header
+{
+background:#339966;
+    height      : 20%;
+    text-align  :center;
+	font-size:48px;
+    padding-top : 20px;
+}
+#sidebar{
+background:#33CC66;
+    float       : left;
+    height      : 400px;
+    width       :20%;
+    text-align  :center;
+    padding-top : 20px;
+ 
+}
+#content
+{
+background:#66CC66;
+    float       : right;
+    width       : 80%;
+    height      : 400px;
+    font-size   : 25px;
+    text-align  :left;
+    padding-top : 20px;
+}
+  </style>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Atika Ilma Yani</title>
+</head>
+<body>
+ 
+ <div id="header">
+    <h2>Biodata</h2>
+  </div>
+   <div id="sidebar">
+   <img src="foto/bebas4x6mera.jpg" width="200px" class="circle" />
+  </div>
+  <div id="content">
+ <table>
+ <tr>
+ <th>Nama</th>
+ <th width="50px"></th>
+ <th>:</th>
+ <th>Atika Ilma Yani</th>
+ </tr>
+ <tr>
+  <th>Umur</th>
+ <th width="50px"></th>
+ <th>:</th>
+ <th>21 Tahun</th>
+ </tr>
+ <tr>
+  <th>Asal</th>
+ <th width="50px"></th>
+ <th>:</th>
+ <th>Surabaya</th>
+ </tr>
+ </table>
+ <p> Perkenalkan nama saya Atika Ilma Yani Asal dari Surabaya Umur 21 Tahun </p>
+<p id="halo">Selamat Pagi</p>
+<button onclick="Ganti()" style=" background-color:#FFFF00; width:120px; height:80px ">KLIK DISINI</button>
+ <script>
+ 
+function Ganti() {
+document.getElementById("halo").innerHTML = "Salam Kenal!";
+}
+</script>
+<a href="halo.php">/Masuk Ke halaman</a>
+  </div>
+  
+
+</body>
+</html>
